0a01f3c94112d795b0d7caa64418fb3aa196961c
Chris Anderson <chris.d.anderson87@gmail.com>
2023-10-07 11:35:28 +0700

    added in static buttons

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// buttons
+const rockButton = document.createElement("button");
+const paperButton = document.createElement("button");
+const scissorsButton = document.createElement("button");
+
+// logic for rock
+rockButton.textContent = "Rock";
+document.body.appendChild(rockButton);
+
+// logic for paper
+paperButton.textContent = "Paper";
+document.body.appendChild(paperButton);
+
+// logic for scissors
+scissorsButton.textContent = "Scissors";
+document.body.appendChild(scissorsButton);
