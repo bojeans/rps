0b9671b7e1dd03eaaa67e3873e8986ba6892b29c
Chris Anderson <chris.d.anderson87@gmail.com>
2023-10-07 17:47:43 +0700

    created ordered list for scoring and tested html format

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // html skeleton
+
+// controls the rock/paper/scissors div
 const gameOptions = document.createElement("div");
 gameOptions.className = "main";
 document.body.appendChild(gameOptions);
 
-const scoreCount = document.createElement("div");
+const scoreCount = document.createElement("ol");
 scoreCount.className = "score";
 document.body.appendChild(scoreCount);
 
@@ -11,9 +13,19 @@ document.body.appendChild(scoreCount);
 const rockButton = document.createElement("button");
 const paperButton = document.createElement("button");
 const scissorsButton = document.createElement("button");
+
+// Ordered list logic
+const playerItem = document.createElement("li");
 const player = document.createTextNode("Player Score");
+playerItem.appendChild(player);
+
+const computerItem = document.createElement("li");
 const computer = document.createTextNode("Computer Score");
+computerItem.appendChild(computer);
+
+const totalGamesItem = document.createElement("li");
 const totalGames = document.createTextNode("Total Games Played");
+totalGamesItem.appendChild(totalGames);
 
 // logic for rock
 rockButton.textContent = "Rock";
@@ -43,6 +55,9 @@ scissorsButton.addEventListener("click", () => {
 gameOptions.appendChild(rockButton);
 gameOptions.appendChild(paperButton);
 gameOptions.appendChild(scissorsButton);
+scoreCount.appendChild(playerItem);
+scoreCount.appendChild(computerItem);
+scoreCount.appendChild(totalGamesItem);
 
 // Game Logic
 const winningLogic = {
