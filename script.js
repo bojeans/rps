fde3564c1b9801b0b4ed96e48d7e9e9b8041ceba
Chris Anderson <chris.d.anderson87@gmail.com>
2023-10-07 12:06:10 +0700

    added individual classes and test with css

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// html skeleton
+const mainDiv = document.createElement("div");
+mainDiv.className = "main";
+document.body.appendChild(mainDiv);
+
 // buttons
 const rockButton = document.createElement("button");
 const paperButton = document.createElement("button");
@@ -5,6 +10,7 @@ const scissorsButton = document.createElement("button");
 
 // logic for rock
 rockButton.textContent = "Rock";
+rockButton.className = "rock";
 document.body.appendChild(rockButton);
 rockButton.addEventListener("click", () => {
   alert("Rock clicked!");
@@ -12,6 +18,7 @@ rockButton.addEventListener("click", () => {
 
 // logic for paper
 paperButton.textContent = "Paper";
+paperButton.className = "paper";
 document.body.appendChild(paperButton);
 paperButton.addEventListener("click", () => {
   alert("Paper clicked!");
@@ -19,7 +26,12 @@ paperButton.addEventListener("click", () => {
 
 // logic for scissors
 scissorsButton.textContent = "Scissors";
+scissorsButton.className = "scissors";
 document.body.appendChild(scissorsButton);
 scissorsButton.addEventListener("click", () => {
   alert("Scissors clicked!");
 });
+
+mainDiv.appendChild(rockButton);
+mainDiv.appendChild(paperButton);
+mainDiv.appendChild(scissorsButton);
