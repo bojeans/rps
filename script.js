8e7b6087279059a352a22b9f1f24fdcf45b6323f
Chris Anderson <chris.d.anderson87@gmail.com>
2023-10-07 11:45:15 +0700

    tested alerts work when clicking buttons

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ const scissorsButton = document.createElement("button");
 // logic for rock
 rockButton.textContent = "Rock";
 document.body.appendChild(rockButton);
+rockButton.addEventListener("click", () => {
+  alert("Rock clicked!");
+});
 
 // logic for paper
 paperButton.textContent = "Paper";
 document.body.appendChild(paperButton);
+paperButton.addEventListener("click", () => {
+  alert("Paper clicked!");
+});
 
 // logic for scissors
 scissorsButton.textContent = "Scissors";
 document.body.appendChild(scissorsButton);
+scissorsButton.addEventListener("click", () => {
+  alert("Scissors clicked!");
+});
